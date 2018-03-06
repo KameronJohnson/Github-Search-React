15887eab7fb1f890d63ef0bcb4f9e6dedd25becc
kameron johnson <kameronjohnson@gmail.com>
2018-03-05 21:08:09 -0800

    add axios, add Form component that successfully updates userName state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+
+class Form extends Component {
+
+  state = {
+    userName: ''
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" 
+        value={this.state.userName}
+        placeholder="Username"
+        onChange={event => this.setState({userName: event.target.value})}
+        />
+        <button type="submit">Search</button>
+      </form>
+    )
+  }
+
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Github Search</h1>
         </header>
-        <p className="App-intro">
-
-          <span className="pink">To get started, edit <code>src/App.js</code> and save to reload.</span>
-        </p>
+        <Form />
       </div>
     );
   }
