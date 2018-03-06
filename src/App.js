3feb44b897e595070baa72e3098bf33c78ded6fc
kameron johnson <kameronjohnson@gmail.com>
2018-03-05 22:11:22 -0800

    successfully render github user image & name via get request to github api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import axios from "axios";
 import './App.css';
 
+const UserCard = props => {
+  
+    return (
+      <div>
+        <img className="user-card-img" 
+        src={props.avatar_url} 
+        alt={`Github image of ${props.name}`}/>
+        <p>{props.name}</p>
+      </div>
+    )
+  
+  }
+  
+const UserCardList = props => {
+
+  return (
+    <div>
+      {
+        props.userCards.map(userCard => 
+          <UserCard key={userCard.name} {...userCard} />
+        )
+      }
+    </div>
+  )
+
+}
+
 class Form extends Component {
 
   state = {
@@ -10,6 +37,14 @@ class Form extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
+
+    axios
+      .get(`https://api.github.com/users/${this.state.userName}`)
+      .then(response => {
+        this.props.onSubmit(response.data);
+        //clear username
+        this.setState({ userName: ''});
+      });
   }
 
   render() {
@@ -24,17 +59,28 @@ class Form extends Component {
       </form>
     )
   }
-
 }
 
 class App extends Component {
+
+  state = {
+    userCards: []
+  }
+
+  addNewUserCard = userCardInfo => {
+    this.setState(prevState => ({
+      userCards: prevState.userCards.concat(userCardInfo)
+    }));
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Github Search</h1>
         </header>
-        <Form />
+        <Form onSubmit={this.addNewUserCard}/>
+        <UserCardList userCards={this.state.userCards}/>
       </div>
     );
   }
